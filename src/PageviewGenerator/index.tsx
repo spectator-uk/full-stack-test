e129d89856edac3f5679d4537bc9969f498eedc6
Ed Stephens <estephens@pressholdings.com>
2020-06-29 11:43:04 +0100

    Remove div

--- src/PageviewGenerator/index.tsx
+++ src/PageviewGenerator/index.tsx
@@ -11,7 +11,7 @@ export const PageviewGenerator: React.FC = () => {
       </button>
 
       {pageview && (
-        <div>
+        <>
           <h3>Last pageview</h3>
           <p>Event ID: {pageview?.id}</p>
           <p>Event date: {pageview?.created_at.toISOString()}</p>
@@ -20,7 +20,7 @@ export const PageviewGenerator: React.FC = () => {
           <p>Page tags: {pageview.page?.tags?.join(', ')}</p>
           <p>User ID: {pageview?.user?.id}</p>
           <p>User joined: {pageview?.user?.created_at.toISOString()}</p>
-        </div>
+        </>
       )}
     </>
   )
